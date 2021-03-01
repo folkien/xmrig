364a2a30c3b90c054043136357d44d8feb85f6e2
Sławomir Paszko <folkus@gmail.com>
2021-03-01 14:44:30 +0100

    Fixes.

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ sudo apt libuv1.dev install hwloc libhwloc-dev   -yf
 mkdir -p build && cd build
 CMAKE="-O2 -march=native -mtune=native" cmake ..
 make -j9
+mkdir -p /opt/xmrig/
+cp -rfv xmrig /opt/xmrig/
